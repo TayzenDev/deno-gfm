c5a776b36291617521468233540fb0ca244858da
TayzenDev <110170236+TayzenDev@users.noreply.github.com>
2024-11-18 12:01:49 +0100

    feat: making youtube iframe lazy

--- mod.ts
+++ mod.ts
@@ -36,7 +36,7 @@ function youtubeLinkToIframe(youtubeUrl: string): string | null {
 
   if (match) {
     const videoId = match[1];
-    return `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    return `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" loading="lazy" allowfullscreen></iframe>
 `;
   }
 
